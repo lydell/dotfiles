509204efbf36a21465e0e6174d00050280b69890
Simon Lydell <simon.lydell@gmail.com>
2016-01-10 13:01:16 +0100

    VimFx: Update config file boilerplate

--- .vimfx/bootstrap.js
+++ .vimfx/bootstrap.js
@@ -5,11 +5,18 @@ function startup() {
   let apiPref = 'extensions.VimFx.api_url'
   let apiUrl = Services.prefs.getComplexValue(apiPref, Ci.nsISupportsString).data
   Cu.import(apiUrl, {}).getAPI(vimfx => {
-    let path = __SCRIPT_URI_SPEC__.replace('bootstrap.js', 'vimfx.js')
+    let path = __SCRIPT_URI_SPEC__.replace('bootstrap.js', 'config.js')
     let scope = {Cc, Ci, Cu, vimfx}
     Services.scriptloader.loadSubScript(path, scope, 'UTF-8')
   })
+  Cc['@mozilla.org/globalmessagemanager;1']
+    .getService(Ci.nsIMessageListenerManager)
+    .loadFrameScript('chrome://vimfx-config/content/frame.js', true)
 }
-function shutdown() {}
+function shutdown() {
+  Cc['@mozilla.org/globalmessagemanager;1']
+    .getService(Ci.nsIMessageListenerManager)
+    .removeDelayedFrameScript('chrome://vimfx-config/content/frame.js')
+}
 function install() {}
 function uninstall() {}
