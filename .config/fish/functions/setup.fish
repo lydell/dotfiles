0fe9cbff71359ebe8f40cc8d7889d5a6f114759d
Simon Lydell <simon.lydell@gmail.com>
2017-09-25 13:44:19 +0200

    fish: Update git abbreviations

--- .config/fish/functions/setup.fish
+++ .config/fish/functions/setup.fish
@@ -13,7 +13,6 @@ function setup
   abbr gbr 'git branch'
   abbr gci 'git commit'
   abbr gco 'git checkout'
-  abbr gd 'git diff'
   abbr gl 'git log'
   abbr glol 'git log --all --graph --decorate --oneline'
   abbr glols 'git log --all --graph --decorate --oneline --simplify-by-decoration'
@@ -22,6 +21,6 @@ function setup
   abbr gp 'git pull'
   abbr gr 'git rebase'
   abbr gs 'git show'
-  abbr gst 'git status'
+  abbr gst 'git stash'
   abbr gu 'git push'
 end
