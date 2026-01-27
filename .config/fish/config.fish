20d50ca0b876d8a34d5249d371f341573eb99a8c
Simon Lydell <simon.lydell@gmail.com>
2026-01-27 20:33:50 +0100

    fish: Fix setting of ELM_WATCH_OPEN_EDITOR env var

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,9 +12,9 @@ set tide_git_truncation_length 48
 function __nvm_auto --on-variable PWD
   nvm use --silent 2>/dev/null
   if test -d .idea || test -d ../.idea
-    set -x ELM_WATCH_OPEN_EDITOR '/Applications/WebStorm.app/Contents/MacOS/webstorm --line "$line" --column "$((column - 1))" "$file"'
+    set -gx ELM_WATCH_OPEN_EDITOR '/Applications/WebStorm.app/Contents/MacOS/webstorm --line "$line" --column "$((column - 1))" "$file"'
   else
-    set -x ELM_WATCH_OPEN_EDITOR 'code --goto "$file:$line:$column"'
+    set -gx ELM_WATCH_OPEN_EDITOR 'code --goto "$file:$line:$column"'
   end
 end
 __nvm_auto
